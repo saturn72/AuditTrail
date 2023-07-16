79c2ac7bccecf94bbe8013eec04e729741027b15
Roi Shabtai <roi@saturn72.com>
2023-07-16 11:56:56 +0300

    donwgrade packages to latest stable

--- Sample/Server/Domain/Product.cs
+++ Sample/Server/Domain/Product.cs
@@ -5,6 +5,7 @@
         public int Id { get; init; }
         public string? Name { get; set; }
         public decimal Price { get; set; }
+        public string? Description { get; set; }
         public List<Category>? Categories { get; set; } = new();
     }
 }
